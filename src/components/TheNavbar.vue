77ad6b48e812a7d1f443ac6387d7508605dbd683
leadingtw273 <leadingtw@gmail.com>
2020-05-08 00:18:31 +0800

    fix logo tag to button

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -3,11 +3,13 @@
     <v-img class="image" src="../assets/index.jpg" max-height="800px"></v-img>
     <v-container class="pa-10" fluid>
       <div class="d-flex justify-space-between">
-        <router-link :to="{ name: 'Home' }" class="d-flex">
-          <v-img src="../assets/TSVS_logo.png" height="80px" width="80px" contain></v-img>
-          <div class="d-flex flex-column align-self-center ml-4">
-            <span class="white--text display-1">台灣血管外科學會</span>
-            <span class="white--text">Taiwan Society for Vascular Surgery</span>
+        <router-link :to="{ name: 'Home' }" tag="button">
+          <div class="d-flex">
+            <v-img src="../assets/TSVS_logo.png" height="80px" width="80px" contain></v-img>
+            <div class="d-flex flex-column align-self-center ml-4">
+              <span class="white--text display-1">台灣血管外科學會</span>
+              <span class="white--text">Taiwan Society for Vascular Surgery</span>
+            </div>
           </div>
         </router-link>
         <div class="d-flex align-self-center">

383e6c72d90f051914069e939032e9e0669d1983
leadingtw273 <leadingtw@gmail.com>
2020-05-19 14:39:35 +0800

    add tag to admin

--- src/views/Admin/Content/CooperativeSocietyAndSponsor.vue
+++ src/views/Admin/Content/CooperativeSocietyAndSponsor.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="my-12 px-12"></div>
+  <div>
+    <the-tag-group :contentList="contentList" :contentItemIndex.sync="contentItemIndex"></the-tag-group>
+
+    <div class="my-12 px-12" v-if="contentItemIndex === 0">合作學會</div>
+    <div class="my-12 px-12" v-if="contentItemIndex === 1">贊助廠商</div>
+  </div>
 </template>
 
 <script>
+import TheTagGroup from "../../../components/TheTagGroup";
 export default {
-  name: "CooperativeSocietyAndSponsor"
+  name: "CooperativeSocietyAndSponsor",
+  components: { TheTagGroup },
+  data() {
+    return {
+      contentItemIndex: 0,
+      contentList: [
+        { text: "合作學會", tag: "#合作學會" },
+        { text: "贊助廠商", tag: "#贊助廠商" }
+      ]
+    };
+  }
 };
 </script>
 

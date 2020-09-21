595f7f96d2850d01f58e11d6a7c2d304758742ae
leadingtw273 <leadingtw@gmail.com>
2020-09-21 14:31:14 +0800

    fix: 修復頁面左上角 Home Link 圖示出現白框的問題

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -3,13 +3,11 @@
     <v-img class="image" src="../assets/index.jpg" max-height="800px"></v-img>
     <v-container class="pa-10" fluid>
       <div class="d-flex justify-space-between">
-        <router-link :to="{ name: 'Home' }" tag="button">
-          <div class="d-flex">
-            <v-img src="../assets/TSVS_logo.png" height="80px" width="80px" contain></v-img>
-            <div class="d-flex flex-column align-self-center ml-4">
-              <span class="white--text display-1 text-shadow">台灣血管外科學會</span>
-              <span class="white--text text-shadow">Taiwan Society for Vascular Surgery</span>
-            </div>
+        <router-link class="d-flex" :to="{ name: 'Home' }" tag="a" style="text-decoration:none;">
+          <v-img src="../assets/TSVS_logo.png" height="80px" width="80px" contain></v-img>
+          <div class="d-flex flex-column align-self-center ml-4">
+            <span class="white--text display-1 text-shadow">台灣血管外科學會</span>
+            <span class="white--text text-shadow">Taiwan Society for Vascular Surgery</span>
           </div>
         </router-link>
         <div class="d-flex align-self-center">

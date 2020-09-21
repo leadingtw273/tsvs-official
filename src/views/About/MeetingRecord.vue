1ceb66b8517b7479585711ae209343a468825e44
leadingtw273 <leadingtw@gmail.com>
2020-09-21 19:28:54 +0800

    Revert "feat: 刪除 TheLoginRequest Component"
    This reverts commit 71f88c739a80bda70b4dd4662a1e589360a502d0.

--- src/views/About/MeetingRecord.vue
+++ src/views/About/MeetingRecord.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="d-flex my-12 px-12">
-    <v-simple-table class="transparent">
-      <template v-slot:default>
-        <tbody>
-          <tr>
-            <td class="title">2019-10-15</td>
-            <td class="title">第八屆第一次理監事會議紀錄</td>
-            <td><btn-more class="ml-3"></btn-more></td>
-          </tr>
-          <tr>
-            <td class="title">2019-09-11</td>
-            <td class="title">第八屆第一次會員大會紀錄</td>
-            <td><btn-more class="ml-3"></btn-more></td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <the-login-request>
+      <v-simple-table class="transparent">
+        <template v-slot:default>
+          <tbody>
+            <tr>
+              <td class="title">2019-10-15</td>
+              <td class="title">第八屆第一次理監事會議紀錄</td>
+              <td><btn-more class="ml-3"></btn-more></td>
+            </tr>
+            <tr>
+              <td class="title">2019-09-11</td>
+              <td class="title">第八屆第一次會員大會紀錄</td>
+              <td><btn-more class="ml-3"></btn-more></td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </the-login-request>
   </div>
 </template>
 
 <script>
+import TheLoginRequest from "../../components/TheLoginRequest";
+
 export default {
-  name: "MeetingRecord"
+  name: "MeetingRecord",
+  components: {
+    TheLoginRequest
+  }
 };
 </script>
 
